d64fecae09fde3560dca2e173a01b9c0be242a3f
murata0531 <coless115@gmail.com>
2021-08-02 16:27:04 +0900

    キーワードテーブル

--- opt/main.py
+++ opt/main.py
@@ -9,17 +9,6 @@ import tkinter as tk
 import tkinter.simpledialog as sd
 import math
 
-##############################
-# 各種ファイルのインポート
-##############################
-
-
-
-
-##############################
-#      メイン
-##############################
-
 #グローバル変数
 fname = '=====text2.txt' #ソースコードのファイル名
 _spt = 0 #ソース読取ポインタ
@@ -57,3 +46,151 @@ localVarSize = [] #各関数内のローカル変数のサイズ
 fnnoList = [] #各行が何番の関数に属しているか
 ifEndList = [] #ifに対応するendの行番号のリスト
 breakList = []
+
+#####################################
+#     トークンの種類、中間コード        #
+#####################################
+
+While = 0
+Func = 1
+If = 2
+Else = 3
+Elif = 4
+End = 5
+Plus = 6
+Minus = 7
+Mult = 8
+Div = 9
+Assign = 10
+Comma = 11
+Db1Q = 12
+Dot = 13
+Equal = 14
+NotEq = 15
+Less = 16
+LessEq = 17
+Great = 18
+GreatEq = 19
+EOT = 20
+EOL = 21
+EOF = 22
+LParen = 23
+RParen = 24
+LBracket = 25
+RBracket = 26
+Mod = 27
+Doll = 28
+Under = 29
+Sg1Q = 30
+Comment = 31
+PlusEq = 32
+MinusEq = 33
+MultEq = 34
+DivEq = 35
+Db1Num = 36
+Str = 37
+Digit = 38
+Letter = 39
+Not = 40
+Ident = 41
+Print = 42
+For = 43
+To = 44
+Break = 45
+Return = 46
+Exit = 47
+And = 48
+Or = 49
+Fcall = 50
+Lvar = 51
+Gvar = 52
+Pow = 53
+Step = 54
+Input = 55
+Rfile = 56
+Wfile = 57
+Toint = 58
+Sin = 59
+Cos = 60
+Tan = 61
+Dim = 62 #配列の定義
+Dvar = 63 # 配列変数の中間コード用
+IDiv = 64 #整数の商
+Dsin = 65
+Dcos = 66
+Dtan = 67
+
+cEOL = chr(EOL)
+
+#キーワードテーブル
+
+KWTbl =\
+    [['while',  While, ''],
+     ['func',   Func,   ''],
+     ['if',     If,     ''],
+     ['else',   Else,   ''],
+     ['elif',   Elif,   ''],
+     ['end',    End,    ''],
+     ['+',      Plus,   ''],#6
+     ['-',      Minus,  ''],
+     ['*',      Mult,   ''],
+     ['/',      Div,    ''],
+     ['=',      Assign, ''],#10
+     [',',      Comma,  ''],
+     ['"',      DblQ,   ''],
+     ['.',      Dot,    ''],
+     ['==',     Equal,  ''],#14
+     ['!=',     NotEq,  ''],
+     ['<',      Less,   ''],
+     ['<=',     LessEq, ''],
+     ['>',      Great,  ''],
+     ['>=',     GreatEq,    ''],
+     ['EOT',    EOT,    ''],#20
+     ['EOL',    EOL,    ''],
+     ['EOF',    EOF,    ''],
+     ['(',      LParen, ''],
+     [')',      RParen, ''],
+     ['[',      LBracket,   ''],
+     [']',      RBracket,   ''],
+     ['//',     IDiv,    ''], #整数の商
+     ['$',      Doll,   ''],
+     ['_',      Under,  ''],
+     ["'",      SglQ,   ''],#30
+     ['#',     Comment,    ''],#31
+     ['+=',     PlusEq, ''],
+     ['-=',     MinusEq,    ''],
+     ['*=',     MultEq, ''],
+     ['/=',     DivEq,  ''],#35
+     ['DblNum', DblNum, ''],
+     ['Str',    Str,    ''],
+     ['Digit',  Digit,  ''],
+     ['Letter', Letter, ''],
+     ['!',      Not,    ''],
+     ['Ident',  Ident,  ''],#41
+     ['print',  Print,  ''],
+     ['for',    For,    ''],
+     ['to',     To,     ''],
+     ['break',  Break,  ''],
+     ['return', Return, ''],
+     ['exit',   Exit,   ''],
+     ['and',    And,    ''],
+     ['or',     Or,     ''],
+     ['Fcall',  Fcall,  ''],
+     ['Lvar',   Lvar,   ''],
+     ['Gvar',   Gvar,   ''],#52
+     ['**',     Pow,    ''],#53
+     ['step',   Step,   ''],#54
+     ['input',  Input,   ''],#55
+     ['rfile',  Rfile,   ''],#56
+     ['wfile',  Wfile,   ''],#57
+     ['toint',  Toint,   ''],#58
+     ['sin',    Sin,   ''],#59
+     ['cos',    Cos,   ''],#60
+     ['tan',    Tan,   ''],#61
+     ['dim',    Dim,   ''],#62
+     ['Dvar',   Dvar,   ''],#63
+     ['%',      Mod,   ''],#64 剰余
+     ['dsin',   Dsin,   ''],
+     ['dcos',   Dcos,   ''],
+     ['dtan',   Dtan,   ''],
+    ]
