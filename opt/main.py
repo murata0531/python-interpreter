e5c09c710f2d7231e9458ce49cc8a7685fb500b7
murata0531 <coless115@gmail.com>
2021-08-12 21:27:22 +0900

    ifを読んだらコールする

--- opt/main.py
+++ opt/main.py
@@ -963,3 +963,24 @@ def chkIfBlock() :
         _line += 1
     #endがないときここへ来る
     raise Exception(errmsg0 + str(ln + 1))
+
+###############################################
+#      while, for, funcを読んだらコールする      #
+###############################################
+
+def chkBlock() :
+    global _line
+    ln = _line
+    _line += 1
+    while _line < len(InterCode) :
+        ic = InterCode[_line][0]
+        if ic == While or ic == For :
+            chkBlock()
+        elif ic == If :
+            chkIfBlock()
+        elif ic == End :
+            return
+        elif ic == Elif or ic == Else or ic == Func:
+            raise Exception(errmsg0 + str(_line + 1))
+        _line += 1
+    raise Exception(errmsg0 + str(ln + 1))
