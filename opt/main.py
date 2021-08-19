e5ac2a54d3a843d5c739b3f40072ca4ddaa55881
murata0531 <coless115@gmail.com>
2021-08-19 23:41:26 +0900

    ()に囲まれた式を評価し、opstackに積む

--- opt/main.py
+++ opt/main.py
@@ -1818,3 +1818,16 @@ def execIfBlock() :
     else : #cd = End
         setlpt2(ed + 1, 0)
         return
+
+########################################
+#   ()に囲まれた式を評価し、opstackに積む 　#
+#   左括弧を指した状態でコール             #
+########################################
+
+def parenExp() :
+    if not checkic(LParen) :
+        raise Exception(errmsg0 + str(_line + 1))
+    expression()
+    if not checkic(RParen) :
+        raise Exception(errmsg0 + str(_line + 1))
+    return
